e9fa0f4193d83cdea6a3588b6ed3dadbf29ae6b9
Calvin Green <calvingreen94@aol.com>
2021-12-26 15:24:05 -0500

    initial trx-eth commit
    crosschaining /asynchronous transactions

--- LabUnreal_8677.py
+++ LabUnreal_8677.py
@@ -40,7 +40,11 @@ daisy = 'dAIsy'
 app = Flask(__name__)
 app.secret_key = "You Come t0 m3 0n th3 dai 0f mah dAUghterZZzz wedDinng BITCH!"
 pub_key ='pk_live_2pO0yUvt9xKyjAo9rca8Vkc600FWtgJuqZ'
-
+infura_url = "https://ropsten.infura.io/v3/89f69d97c5c44c35959cc4d15c0f0531"
+#	 infura_url = 'wss://ropsten.infura.io/ws/v3/89f69d97c5c44c35959cc4d15c0f0531'
+web3 = Web3(Web3.HTTPProvider(infura_url))
+web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+	
 
 class LabUnrealBlockchain:
 
@@ -61,27 +65,41 @@ class LabUnrealBlockchain:
 		import random
 		my_url = ''
 		#opening up connection, downloading the page
-		html_page = requests.get('')
-		soup = soup(html_page.content, 'html.parser')
-		warning = soup.find('div', class_="")
-		contents = warning.findAll('img')
-		content = contents
-		content = str(''.format())
-		# command = takeCommand()
+#		html_page = requests.get('')
+		#soup = soup(html_page.content, 'html.parser')
+		#warning = soup.find('div', class_="")
+		#contents = warning.findAll('img')
+		#content = contents
+		#content = str(''.format())
+		abi='[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"sender","type":"address"},{"name":"recipient","type":"address"},{"name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"value","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"recipient","type":"address"},{"name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"name","type":"string"},{"name":"symbol","type":"string"},{"name":"decimals","type":"uint8"},{"name":"totalSupply","type":"uint256"},{"name":"feeReceiver","type":"address"},{"name":"tokenOwnerAddress","type":"address"}],"payable":true,"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"}]'
+		abi = json.loads(abi)
+		address1 = '0x1a42C1599d0AC239f504F0cbb1C4f55CD2de8bfe'
+		eth_contract = web3.eth.contract(address=address1, abi=abi)	
 
-		mint_acct =''
-		priv_key = PrivateKey(bytes.fromhex(""))
-		receiver1= ''		
+		mint_acct_eth =''
+		priv_key_eth = ''		
+		receiver1_eth= address1	
+
+
+		mint_acct_trx =''
+		priv_key_trx = PrivateKey(bytes.fromhex(""))
+		receiver1_trx= ''	
+
+			
 		block = {'index': len(self.chain) + 1,
-				 'timestamp': str(datetime.datetime.now()),
-				 'proof': proof,
-				 'previous_hash': previous_hash,
-				 'transactions': self.transactions,
-				 'Miner_Minting_Address':mint_acct, 
-				   'receiver':receiver1 ,
-				   'content':content,
-				   'totalSupply':contract.functions.balanceOf('') ,
-				 }
+				'timestamp': str(datetime.datetime.now()),
+				'proof': proof,
+				'previous_hash': previous_hash,
+				'transactions': self.transactions,
+				'Miner_Minting_Address_trx':mint_acct_trx, 
+				'Miner_Minting_Address_eth':mint_acct_eth,				 
+				'receiver_trx':receiver1_trx ,
+				'receiver1_eth':receiver1_eth,
+				   # 'content':content,
+				'totalSupply_trx':contract.functions.balanceOf(address1) ,
+				'totalSupply_trx':contract.functions.balanceOf(receiver1_trx) ,
+				
+				}
 		self.transactions = []	 
 		self.chain.append(block) 
 		return block 
@@ -120,21 +138,28 @@ class LabUnrealBlockchain:
 			block_index += 1
 		return True
 	
-	def add_transaction(self,sender,receiver,amount): # sender = sender receiver = receiver amount = amount
-		mint_acct =''
-		priv_key = PrivateKey(bytes.fromhex(""))
-		# web3.eth.mint_acct = mint_acct
-		receiver1= ''
+	def add_transaction(self,sender_trx,sender_eth,receiver_trx,receiver_eth, amount_trx,amount_eth): # sender = sender receiver = receiver amount = amount
+		mint_acct_trx =''
+		priv_key_trx = PrivateKey(bytes.fromhex(""))
+		# web3.eth.mint_acct_trx = mint_acct_trx
+		receiver1_trx= ''
 
+		mint_acct_eth =''
+		priv_key_eth = ''		
+		receiver1_eth= address1	
 
 		previous_block = blockchain.get_previous_block()
 		previous_proof = previous_block['proof']
 		proof = blockchain.proof_of_work(previous_proof)
 		previous_hash = blockchain.hash(previous_block)	 
-		self.transactions.append({'sender': sender,
-		'receiver':receiver1,
-		'amount':amount,
-		'minter':mint_acct})
+		self.transactions.append({'sender_trx': sender_trx})
+		self.transactions.append({'sender_eth': sender_eth})			
+		'receiver_trx':receiver1_trx,
+		'receiver_eth':receiver1_eth,		
+		'amount_trx':amount_trx,
+		'amount_eth':amount_eth,
+		'minter_trx':mint_acct_trx,
+		'minter_eth':mint_acct_eth,		})
 		previous_block = self.get_previous_block()
 		return previous_block['index'] + 1 
 
@@ -206,7 +231,7 @@ def gen_add():
 	add = {}
 	data = {}
 
-	message['message'] = 'NODE :"{}":   New Address/Private key. Use This To get paid for content uploads. ["({})"], [{}]'.format(node,new_cl,warning)
+	message['message'] = 'NODE :"{}":  TRON New Address/Private key. Use This To get paid for content uploads. ["({})"], [{}]'.format(node,new_cl,warning)
 	add['address'] = str(new_cl)
 	data['status'] = 200
 	data['data'] = message
@@ -222,46 +247,61 @@ def mine_block():
 	client = Tron() 
 	client1 = Tron(network='nile')
 	amount =  1_000_000 
-	mint_acct =''
-	priv_key = PrivateKey(bytes.fromhex(""))
-	receiver1= '' #-> liquidity pool
+	mint_acct_trx =''
+	priv_key_trx = PrivateKey(bytes.fromhex(""))
+	receiver1_trx= '' #-> liquidity pool
 	previous_block = blockchain.get_previous_block()
 	previous_proof = previous_block['proof']
 	proof = blockchain.proof_of_work(previous_proof)
 	previous_hash = blockchain.hash(previous_block)
+
+	address = 'contract address'
+	abi='[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"}]'
+	dai = web3.eth.contract(address=address, abi=abi)		
+	mint_acct ='' #<= input acct address 
+	mint_key = '' #<= input acct priv. key
+
+	web3.eth.mint_acct = mint_acct
+	receiver1= ''
+	amount_eth =  web3.toWei(float(.003),'ether')
+	mint_tx =dai.functions.transfer(receiver1, 0x00000053999999).buildTransaction({'chainId': 3, 'gas':8000000, 'nonce':  web3.eth.getTransactionCount(mint_acct)})
+	signed_tx = web3.eth.account.signTransaction(mint_tx, mint_key)
+	tx_hash_eth=web3.eth.sendRawTransaction(signed_tx.rawTransaction)
 	# amount =  web3.toWei(float(.0003),'ether')
 
-	txn = (client.trx.transfer(mint_acct,receiver1,1_000)
+	txn = (client.trx.transfer(mint_acct_trx,receiver1_trx,1_000)
 		.memo("did it work")
 		.build()
-		.sign(priv_key)
+		.sign(priv_key_trx)
 		)
 	tx_hash = txn.txid
 	txn.broadcast()
 	contract = client.get_contract("") 
 	txn1 = (
-		contract.functions.transfer(receiver1, 1_000_000)
-		.with_owner(mint_acct) 
+		contract.functions.transfer(receiver1_trx, 1_000_000)
+		.with_owner(mint_acct_trx) 
 		.build()
-		.sign(priv_key)
+		.sign(priv_key_trx)
 		)
 	tx_hash = txn1.txid	
 	# tx_hash = hash(tx_hash)+hash(tx_hash)**2
 	txn.broadcast()
-	blockchain.add_transaction(sender = mint_acct , receiver=receiver1,amount=1)
+	blockchain.add_transaction(sender_eth = mint_acct_eth,sender_trx = mint_acct_trx ,receiver_eth=receiver1_eth, receiver_trx=receiver1_trx,amount_trx=1,amount_eth=amount)
 	block = blockchain.create_block(proof, previous_hash)
 	response = {'message': 'Congratulations, you just mined a block!',
 				'index': block['index'],
 				'timestamp': block['timestamp'],
 				'proof': block['proof'],
 				'previous_hash': block['previous_hash'],
+				'ETHEREUM_HASH': tx_hash_eth,				
 				'LFR-TRX_HASH': tx_hash,
 				'transactions': block['transactions'],
-				'receiver':receiver1,
+				'receiver_trx':receiver1_trx,
+				'receiver_eth':receiver1_eth,
 				'content':block['content']} 
 	message = {} 
 	data = {}
-	message['message'] = 'Congratulations, you just mined  block {} at {}!, \n LabUnreal-TRX HASH {}, Proof of work {}, previous LabUnreal hash {}, \n transactions{}, \n RECEIVING MINTER {}'.format(block['index'],block['timestamp'],web3.toHex(tx_hash),block['proof'],block['previous_hash'],block['transactions'],receiver1) 
+	message['message'] = 'Congratulations, you just mined  block {} at {}!, \n LabUnreal-TRX HASH {}, ETHEREUM HASH {},Proof of work {}, previous LabUnreal hash {}, \n transactions{}, \n RECEIVING TRX pool {}, RECEIVING ETH pool {} '.format(block['index'],block['timestamp'],web3.toHex(tx_hash),tx_hash_eth,block['proof'],block['previous_hash'],block['transactions'],receiver1_trx,receiver1_eth) 
 	data['status']= 200
    
 	data['data'] = message
